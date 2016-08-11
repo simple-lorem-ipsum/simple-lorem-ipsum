edab08aba102b82d655f23506e4c1449d413a626
Reiner Kempkes <kempkes.reiner@gmail.com>
2016-08-11 23:06:03 +0200

    added .editorconfig and reformatted files

--- src/background.js
+++ src/background.js
@@ -1,37 +1,37 @@
 // ugly chrome compatibility layer
 if (typeof browser === 'undefined') {
-    var browser = chrome;
+  var browser = chrome;
 }
 
 let translate = browser.i18n.getMessage;
 
 // notify content script
 function insertLoremIpsum() {
-    browser.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        browser.tabs.sendMessage(tabs[0].id, {status: 'insertLoremIpsum'});
-    });
+  browser.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    browser.tabs.sendMessage(tabs[0].id, {status: 'insertLoremIpsum'});
+  });
 }
 
 // register hotkey
 browser.commands.onCommand.addListener((command) => {
-    if (command === 'insert-lorem-ipsum') {
-        insertLoremIpsum();
-    }
+  if (command === 'insert-lorem-ipsum') {
+    insertLoremIpsum();
+  }
 });
 
 // create context menu
 browser.contextMenus.create({
-    id: 'contextmenuInsertLoremIpsum',
-    title: translate('contextmenuInsertLoremIpsum'),
-    contexts: ['editable']
+  id: 'contextmenuInsertLoremIpsum',
+  title: translate('contextmenuInsertLoremIpsum'),
+  contexts: ['editable']
 });
 
 // create context menu handler
 browser.contextMenus.onClicked.addListener((info) => {
-    if (
-        info.menuItemId === 'contextmenuInsertLoremIpsum' &&
-        info.editable
-    ) {
-        insertLoremIpsum();
-    }
+  if (
+    info.menuItemId === 'contextmenuInsertLoremIpsum' &&
+    info.editable
+  ) {
+    insertLoremIpsum();
+  }
 });
--- src/content-script.js
+++ src/content-script.js
@@ -1,24 +1,24 @@
 // ugly chrome compatibility layer
 if (typeof browser === 'undefined') {
-    var browser = chrome;
+  var browser = chrome;
 }
 
 const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, ' +
-    'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' +
-    'At vero eos et accusam et justo duo dolores et ea rebum. ' +
-    'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ';
+  'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ' +
+  'At vero eos et accusam et justo duo dolores et ea rebum. ' +
+  'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ';
 
 function insertLoremIpsum() {
 
-    let node = document.activeElement;
+  let node = document.activeElement;
 
-    if (node.tagName === 'INPUT' || node.tagName === 'TEXTAREA') {
-        node.value += LOREM_IPSUM;
-    }
+  if (node.tagName === 'INPUT' || node.tagName === 'TEXTAREA') {
+    node.value += LOREM_IPSUM;
+  }
 }
 
 browser.runtime.onMessage.addListener((request) => {
-    if (request.status === 'insertLoremIpsum') {
-        insertLoremIpsum();
-    }
+  if (request.status === 'insertLoremIpsum') {
+    insertLoremIpsum();
+  }
 });
